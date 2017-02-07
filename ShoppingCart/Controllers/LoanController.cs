d1b47dd4ffbdad30f11ff90ae9f0006507f9d684
Jessica Brisson <brissonjess@gmail.com>
2017-02-06 21:36:52 -0700

    display available item form
    working on getting value from form and sending information to
    LoanController

--- ShoppingCart/Controllers/LoanController.cs
+++ ShoppingCart/Controllers/LoanController.cs
@@ -10,11 +10,43 @@ namespace ShoppingCart.Controllers
     public class LoanController : Controller
     {
         private ShoppingDbContext db = new ShoppingDbContext();
+
+        ////////////////////////////////////////////////
+        ///////////////  View Methods   ////////////////
+        ////////////////////////////////////////////////
         // GET: Loan
         public ActionResult Index()
         {
-            
-            return View(db.Items.ToList());
+            return View(Get_Available_Items());
         }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        ////////////////////////////////////////////////
+        ///////////////  CRUD Methods   ////////////////
+        ////////////////////////////////////////////////
+        public List<Item> Get_Available_Items()
+        {
+            using (var context = db)
+            {
+                var results = from x in context.Items
+                              where x.availableToLoan.Equals(true)
+                              orderby x.item_name ascending
+                              select x;
+                return results.ToList();
+            }
+        }
+        ////////////////////////////////////////////////
+        ///////////////  Cart Methods   ////////////////
+        ////////////////////////////////////////////////
+        [HttpPost]
+        public ActionResult AddToCart(int quantity, int itemid)
+        {
+            
+            ViewBag.Test = "Quantity is: " + quantity + "<br>" + "Item ID is: " + itemid;
+            return View("~/Views/Loan/Index.cshtml");
+        }
+
     }
 }
---
+++ ShoppingCart/Models/POCO/CartItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ShoppingCart.Models.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Spatial;
+
+namespace ShoppingCart.Models.POCO
+{
+    public class CartItem
+    {
+        public int item_id { get; set; }
+
+        public string item_name { get; set; }
+
+        public string item_descr { get; set; }
+
+        public int? quantityOwned { get; set; }
+
+        public int? quantityInstock { get; set; }
+
+        public int? quantityLoaned { get; set; }
+
+        public bool availableToLoan { get; set; }
+
+        public int CartQuantity { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<ShoppingCart_item> ShoppingCart_item { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Item> Items { get; set; }
+    }
+}
